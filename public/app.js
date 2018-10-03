2a75d7a7529e37623e2f09e9807bb2c93d1a9848
arnab majumdar <arnab156@gmail.com>
2018-10-03 14:22:29 -0500

    some changes

--- public/app.js
+++ public/app.js
@@ -110,6 +110,30 @@ $(document).on("click", "#savenote", function () {
   // $("#bodyinput").val("");
 });
 
+$(document).on("click", "#saveNoteModal", function () {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      // Value taken from title input
+      title: $("#savedTitleInput").val(),
+      // Value taken from note textarea
+      body: $("#savedBodyInput").val()
+    }
+  })
+    // With that done
+    .then(function (data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $("#savedNotesModal").empty();
+    });
+});
+
 
 $(document).on("click", "#saveNews", function () {
   // Grab the id associated with the article from the submit button
